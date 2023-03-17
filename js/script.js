9101676253be65018b1c1ba04545a287b358bca0
Werlley5 <110196227+Werlley5@users.noreply.github.com>
2023-03-17 15:25:43 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function menuHamburger() {
+    let menuMobile = document.querySelector('.mobile-menu')
+
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open')
+        document.querySelector('.icon').src = "img/icon-menu.svg";
+    }else {
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "img/icon-menu-close.svg";
+    }
+
+};
